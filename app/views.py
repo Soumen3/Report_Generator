2f4dc91ad9882975fb3b520ba553a52f437d02e3
Soumen3 <soumensamanta112233@gmail.com>
2024-05-31 15:47:29 +0530

    report generating function developed

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import filesForm
 from django.contrib import messages
+import pandas as pd
+from .models import files
 
 # Create your views here.
 def upload_file(request):
@@ -23,4 +25,28 @@ def upload_file(request):
 	
 
 def summary_report(request, pk):
-	return render(request, 'app/summary_report.html')
+	context ={}
+	myFile = files.objects.get(pk=pk)	
+	myFile_path = myFile.file.path
+
+	if myFile_path.endswith('.csv'):
+		df = pd.read_csv(myFile_path)
+	else:
+		df = pd.read_excel(myFile_path)
+	
+	print('dataframe:', df.head(2))
+
+	state_column = next((col for col in df.columns if 'state' in col.lower()), None)
+	dpd_column = next((col for col in df.columns if 'dpd' in col.lower()), None)
+	if state_column is None or dpd_column is None:
+		print('No column related to state or DPD found')
+		messages.error(request, 'No column related to state or DPD found')
+	else:
+		# Group by 'State' and 'DPD' columns and count the number of occurrences
+		summary = df.groupby([state_column, dpd_column]).size().reset_index(name='Count')
+
+		# Convert the DataFrame to HTML to display it in your template
+		context['summary'] = summary.to_html(index=False)
+
+
+	return render(request, 'app/summary_report.html',context)
